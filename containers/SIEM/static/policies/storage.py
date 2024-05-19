edf6b554a8eae6361584742eac86e7a94f39630a
Qifan Deng <qifan.deng@outlook.com>
2024-05-19 15:51:35 +1000

    SIME: storage policy with email notifier

--- containers/SIEM/static/policies/storage.py
+++ containers/SIEM/static/policies/storage.py
@@ -1,15 +1,17 @@
 class StoragePolicy:
 
-    @staticmethod
-    def apply(data):
+    def __init__(self):
+        self.threshold = 50
+
+    def apply(self,
+              data):
         disk_util = data['data']['disk_usage']
 
-        threshold = 50
         not_satisfied = []
         for device, utilization in disk_util.items():
-            if utilization['percent'] > threshold:
+            if utilization['percent'] > self.threshold:
                 not_satisfied.append(device)
         if len(not_satisfied):
             body = '\r\n'.join(not_satisfied)
-            return f'[WARNING] Storage Usage Exceeded {threshold}%', body
+            return f'[WARNING] Storage Usage Exceeded {self.threshold}%', body
         return None, None
