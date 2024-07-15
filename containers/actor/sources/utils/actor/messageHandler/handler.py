0b822e4477bd719557a7adfe6b5880d12ca2fb4f
Qifan Deng <qifan.deng@outlook.com>
2024-07-15 17:44:47 +0800

    iperf: fix

--- containers/actor/sources/utils/actor/messageHandler/handler.py
+++ containers/actor/sources/utils/actor/messageHandler/handler.py
@@ -212,6 +212,7 @@ class ActorMessageHandler:
                 self.basicComponent.debugLogger.debug('Run iperf server')
                 dataRateResult = server.run()
                 if 'error' in dataRateResult.json:
+                    self.basicComponent.debugLogger.warning(dataRateResult)
                     sleep(1)
                     continue
                 data = {
@@ -241,7 +242,7 @@ class ActorMessageHandler:
         data = message.data
         source = message.source
         client = NetProfClient()
-        client.bind_address = self.basicComponent.addr[0]
+        client.bind_address = '0.0.0.0'
         client.server_hostname = source.addr[0]
         client.port = 60000
         client.duration = 2
@@ -253,6 +254,7 @@ class ActorMessageHandler:
                 sleep(3)
                 res = client.run()
                 if 'error' in res.json:
+                    self.basicComponent.debugLogger.warning(res)
                     sleep(1)
                     continue
                 break
--- containers/master/sources/utils/master/messageHandler/profilingHandler.py
+++ containers/master/sources/utils/master/messageHandler/profilingHandler.py
@@ -94,7 +94,7 @@ class ProfilingHandler:
                 self.basicComponent.debugLogger.debug('Run iperf server')
                 dataRateResult = server.run()
                 if 'error' in dataRateResult.json:
-                    print(dataRateResult.json['error'])
+                    self.basicComponent.debugLogger.warning(dataRateResult)
                     sleep(1)
                     continue
                 data = {
@@ -123,7 +123,7 @@ class ProfilingHandler:
         data = message.data
         source = message.source
         client = NetProfClient()
-        client.bind_address = self.basicComponent.addr[0]
+        client.bind_address = '0.0.0.0'
         client.server_hostname = source.addr[0]
         client.port = 60000
         client.duration = 2
@@ -135,6 +135,7 @@ class ProfilingHandler:
                 sleep(3)
                 res = client.run()
                 if 'error' in res.json:
+                    self.basicComponent.debugLogger.warning(res)
                     sleep(1)
                     continue
                 break
