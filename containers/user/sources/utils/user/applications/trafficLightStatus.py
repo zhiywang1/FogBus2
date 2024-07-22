a337cd10cd1dd0e08f1fff03c5183cc8c5a2e4ae
Qifan Deng <qifan.deng@outlook.com>
2024-07-22 16:19:11 +0800

    user: add termination condition for expr

--- containers/user/sources/utils/user/applications/trafficLightStatus.py
+++ containers/user/sources/utils/user/applications/trafficLightStatus.py
@@ -31,7 +31,7 @@ class TrafficLightStatus(ApplicationUserSide):
             self.responseTime.update(time() * 1000 - sent_time)
             self.basicComponent.debugLogger.info(
                 f'{request} # Response time: {self.responseTime.median():.3f} ms')
-            if result > 1000:
+            if request > 1000:
                 import os
                 os._exit(0)
             request += 1
