d09515529d86c8457494ff732a65c4737a883362
Qifan Deng <qifan.deng@outlook.com>
2024-07-22 12:22:33 +0800

    master: WIP

--- containers/master/sources/utils/master/registry/tools/prettyDecision.py
+++ containers/master/sources/utils/master/registry/tools/prettyDecision.py
@@ -7,16 +7,8 @@ from ...scheduler.types.decision import Decision
 def prettyDecision(decision: Decision, registeredActor: RegisteredActors):
     inDict = {}
     taskNameList = decision.user.taskNameList
-    indexSequence = decision.indexSequence
     indexToHostID = decision.indexToHostID
     for i, taskName in enumerate(taskNameList):
-        index = indexSequence[i]
-        try:
-            actorHostID = indexToHostID[index]
-        except Exception:
-            raise Exception(f"Actors: {len(registeredActor)}"
-                            f"index: {index}"
-                            f"indexToHostID: {indexToHostID}"
-                            f"indexSequence: {indexSequence}")
+        actorHostID = indexToHostID[i]
         inDict[taskName] = registeredActor[actorHostID].nameLogPrinting
     return pformat(inDict, indent=8)
